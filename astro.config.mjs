08318323bc2f04138e17fbc2eea69cad11a91896
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-20 22:52:39 +0900

    mdx導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 
 import { defineConfig } from "astro/config"
 import { resolve } from "node:path"
+import mdx from "@astrojs/mdx"
 const __dirname = new URL(".", import.meta.url).pathname
 
 // https://astro.build/config
@@ -14,6 +15,7 @@ export default defineConfig(
           "@": resolve(__dirname, "src")
         }
       }
-    }
+    },
+    integrations: [mdx()]
   }
 )
