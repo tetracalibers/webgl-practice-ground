ef41fc402d0f537f05f4a26e25329d6fddfc2fff
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-23 19:47:21 +0900

    uniformとして時間を送るためのユーティリティ作成

--- src/lib/webgl/shader-data.type.ts
+++ src/lib/webgl/shader-data.type.ts
@@ -1,8 +1,9 @@
 export type UniformMatrix4fv = "uModelViewMatrix" | "uProjectionMatrix" | "uNormalMatrix"
 export type Uniform2fv = "uResolution"
+export type Uniform1f = "uTime"
 
 export type Attribute = "aVertexPosition" | "aVertexNormal" | "aVertexColor" | "aVertexTextureCoords"
-export type Uniform = UniformMatrix4fv | Uniform2fv
+export type Uniform = UniformMatrix4fv | Uniform2fv | Uniform1f
 
 export type AttributeMap = Record<Attribute, number>
 export type UniformMap = Record<Uniform, WebGLUniformLocation | null>
--- src/lib/webgl/uniform-reflect.ts
+++ src/lib/webgl/uniform-reflect.ts
@@ -1,5 +1,5 @@
 import type { Program } from "./program"
-import type { Uniform2fv } from "./shader-data.type"
+import type { Uniform1f, Uniform2fv } from "./shader-data.type"
 
 export class UniformReflect {
   private _gl: WebGL2RenderingContext
@@ -10,11 +10,22 @@ export class UniformReflect {
     this._program = program
   }
 
+  float(uniform: Uniform1f, value: number) {
+    this._gl.uniform1f(this._program.getUniformLocation(uniform), value)
+  }
+
   floatVector2(uniform: Uniform2fv, value: [number, number]) {
     this._gl.uniform2fv(this._program.getUniformLocation(uniform), value)
   }
 
   resolution() {
     this.floatVector2("uResolution", [this._gl.canvas.width, this._gl.canvas.height])
+  }
+
+  time(time: number) {
+    // ミリ秒単位の時間をそのまま渡すと非常に大きな数字になってしまうため、
+    // 千分の一にしてシェーダに送る
+    const v = time * 0.001
+    this.float("uTime", v)
   }
 }
