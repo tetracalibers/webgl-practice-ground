04227f91880298e255027caa3b7ec2be2f2d5c7a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-03-30 18:13:43 +0900

    カテゴリごとに分類表示

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,87 @@
 import { z, defineCollection } from "astro:content"
 
+export const webglCategories = ["shape"] as const
+
+const zWebglStudy = z.object({
+  title: z.string(),
+  category: z.literal("webgl"),
+  subcategory: z.enum(webglCategories)
+})
+
+export const glslCategories = [
+  "color",
+  "basic",
+  "interpolation",
+  "coordinate-system",
+  "plot",
+  "shape",
+  "fractal",
+  "physics",
+  "random",
+  "pattern",
+  "motion",
+  "image-processing",
+  "3d",
+  "application",
+  "simulation"
+] as const
+
+export const glslCategoryDetail: Record<(typeof glslCategories)[number], { title: string }> = {
+  basic: {
+    title: "Basic"
+  },
+  interpolation: {
+    title: "Interpolation"
+  },
+  plot: {
+    title: "Graph"
+  },
+  "coordinate-system": {
+    title: "Coordinate System"
+  },
+  color: {
+    title: "Color"
+  },
+  random: {
+    title: "Random"
+  },
+  shape: {
+    title: "Shape"
+  },
+  "3d": {
+    title: "3D"
+  },
+  application: {
+    title: "アプリケーション"
+  },
+  fractal: {
+    title: "フラクタル"
+  },
+  "image-processing": {
+    title: "画像処理"
+  },
+  motion: {
+    title: "動き"
+  },
+  pattern: {
+    title: "繰り返し"
+  },
+  physics: {
+    title: "物理"
+  },
+  simulation: {
+    title: "シミュレーションとゲーム"
+  }
+}
+
+const zGlslStudy = z.object({
+  title: z.string(),
+  category: z.literal("glsl"),
+  subcategory: z.enum(glslCategories)
+})
+
 const studyCollection = defineCollection({
-  schema: z.object({
-    title: z.string()
-  })
+  schema: z.discriminatedUnion("category", [zWebglStudy, zGlslStudy])
 })
 
 export const collections = {
