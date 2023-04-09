aa9c60e126e1557f5b3002c7e7470ce9be75bb46
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-04-09 17:53:34 +0900

    マウス座標取得をタッチデバイス対応

--- src/lib/control/mouse-coords.ts
+++ src/lib/control/mouse-coords.ts
@@ -1,13 +1,40 @@
 export class MouseCoords {
+  private _rect: DOMRect
   private _coords: [number, number]
 
   constructor(canvas: HTMLCanvasElement, x = 0.5, y = 0.5) {
+    this._rect = canvas.getBoundingClientRect()
     this._coords = [x, y]
-    canvas.addEventListener("mousemove", this.onMouseMove, true)
+
+    canvas.addEventListener("mousemove", this.onMove, { passive: false })
+    canvas.addEventListener("touchmove", this.onMove, { passive: false })
   }
 
-  private onMouseMove = (e: MouseEvent) => {
-    this._coords = [e.offsetX, e.offsetY]
+  private innerPos = (x: number, y: number): [number, number] => {
+    return [x - this._rect.left, y - this._rect.top]
+  }
+
+  private saveTouchPos = (e: TouchEvent) => {
+    if (e.changedTouches.length !== 1) return
+    const finger = e.changedTouches[0]
+    this._coords = this.innerPos(finger.clientX, finger.clientY)
+  }
+
+  private saveMousePos = (e: MouseEvent) => {
+    this._coords = this.innerPos(e.clientX, e.clientY)
+  }
+
+  private isTouchEvent = (e: MouseEvent | TouchEvent): e is TouchEvent => {
+    return e.type.startsWith("touch")
+  }
+
+  private onMove = (e: MouseEvent | TouchEvent) => {
+    e.preventDefault()
+    if (this.isTouchEvent(e)) {
+      this.saveTouchPos(e)
+    } else {
+      this.saveMousePos(e)
+    }
   }
 
   get xy() {
