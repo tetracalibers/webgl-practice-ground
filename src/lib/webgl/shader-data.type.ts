87d4395415bbafef98e1efdcc35dfc4d67dc525c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-04-12 17:02:06 +0900

    uTextureの型を追加

--- src/lib/webgl/shader-data.type.ts
+++ src/lib/webgl/shader-data.type.ts
@@ -1,9 +1,10 @@
 export type UniformMatrix4fv = "uModelViewMatrix" | "uProjectionMatrix" | "uNormalMatrix"
 export type Uniform2fv = "uResolution" | "uMouse"
 export type Uniform1f = "uTime"
+export type Uniform1i = "uTexture"
 
 export type Attribute = "aVertexPosition" | "aVertexNormal" | "aVertexColor" | "aVertexTextureCoords"
-export type Uniform = UniformMatrix4fv | Uniform2fv | Uniform1f
+export type Uniform = UniformMatrix4fv | Uniform2fv | Uniform1f | Uniform1i
 
 export type AttributeMap = Record<Attribute, number>
 export type UniformMap = Record<Uniform, WebGLUniformLocation | null>
