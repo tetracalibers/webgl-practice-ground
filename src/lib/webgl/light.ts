45f8643937d771d41ae9e580d40225da5c49ae7c
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-04-15 17:13:12 +0900

    [Light] ambientColorを受け取るよう修正

--- src/lib/webgl/light.ts
+++ src/lib/webgl/light.ts
@@ -6,12 +6,17 @@ export class Light {
   private _program: Program
   private _direction: [number, number, number] = [0, 0, 0]
   private _invModelMatrix: Matrix4 = Matrix4.identity()
+  private _ambientColor: [number, number, number, number] = [0, 0, 0, 1.0]
 
   constructor(gl: WebGL2RenderingContext, program: Program) {
     this._gl = gl
     this._program = program
 
-    program.setUniformLocations(["uLightDirection", "uInvModelMatrix"])
+    program.setUniformLocations(["uLightDirection", "uInvModelMatrix", "uAmbientColor"])
+  }
+
+  set ambientColor(color: [number, number, number, number]) {
+    this._ambientColor = color
   }
 
   set direction(direction: [number, number, number]) {
@@ -25,8 +30,10 @@ export class Light {
   reflect() {
     const uLightDirection = this._program.getUniformLocation("uLightDirection")
     const uInvModelMatrix = this._program.getUniformLocation("uInvModelMatrix")
+    const uAmbientColor = this._program.getUniformLocation("uAmbientColor")
 
     this._gl.uniform3fv(uLightDirection, this._direction)
+    this._gl.uniform4fv(uAmbientColor, this._ambientColor)
     this._gl.uniformMatrix4fv(uInvModelMatrix, false, this._invModelMatrix.values)
   }
 }
