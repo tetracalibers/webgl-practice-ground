e359a11394998f69f008e3e4ba10a4ebda08c539
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-04-15 14:15:41 +0900

    hsvとrgbを変換するユーティリティ実装

---
+++ src/lib/shape/color.ts
@@ -0,0 +1,22 @@
+export const hsvaToRgba = (h: number, s: number, v: number, a: number) => {
+  let colors = []
+  const th = h % 360
+  // 整数部分と小数部分
+  const i = Math.floor(th / 60)
+  const F = th / 60 - i
+  // よく現れる式を変数化
+  const M = v * (1 - s)
+  const N = v * (1 - s * F)
+  const K = v * (1 - s * (1 - F))
+  if (s === 0) {
+    colors.push(v, v, v, a)
+  } else {
+    // rgb値算出
+    const r = [v, N, M, M, K, v][i]
+    const g = [K, v, v, N, M, M][i]
+    const b = [M, M, K, v, v, N][i]
+    colors.push(r, g, b, a)
+  }
+
+  return colors
+}
