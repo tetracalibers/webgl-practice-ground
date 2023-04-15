bcc6a2530b74205de2bdd992021b644d4322100d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-04-15 17:13:05 +0900

    ambientColorのuniform型を追加

--- src/lib/webgl/shader-data.type.ts
+++ src/lib/webgl/shader-data.type.ts
@@ -1,11 +1,12 @@
 export type UniformMatrix4fv = "uModelViewMatrix" | "uProjectionMatrix" | "uNormalMatrix" | "uInvModelMatrix"
 export type Uniform2fv = "uResolution" | "uMouse"
 export type Uniform3fv = "uLightDirection"
+export type Uniform4fv = "uAmbientColor"
 export type Uniform1f = "uTime"
 export type Uniform1i = "uTexture"
 
 export type Attribute = "aVertexPosition" | "aVertexNormal" | "aVertexColor" | "aVertexTextureCoords"
-export type Uniform = UniformMatrix4fv | Uniform2fv | Uniform1f | Uniform1i | Uniform3fv
+export type Uniform = UniformMatrix4fv | Uniform2fv | Uniform1f | Uniform1i | Uniform3fv | Uniform4fv
 
 export type AttributeMap = Record<Attribute, number>
 export type UniformMap = Record<Uniform, WebGLUniformLocation | null>
