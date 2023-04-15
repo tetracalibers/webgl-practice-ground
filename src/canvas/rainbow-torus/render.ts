94be434b059e0674682e6f0ae1f58e375ae5c7c3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-04-15 17:53:56 +0900

    uniform/attribute位置取得は内部で行われているので削除

--- src/canvas/rainbow-torus/render.ts
+++ src/canvas/rainbow-torus/render.ts
@@ -5,7 +5,6 @@ import fragmentSource from "./index.frag?raw"
 import { Scene } from "@/lib/webgl/scene"
 import { Camera } from "@/lib/webgl/camera"
 import { Transforms } from "@/lib/webgl/transforms"
-import type { Attribute, Uniform } from "@/lib/webgl/shader-data.type"
 import { Matrix4 } from "@/lib/math/matrix"
 import { torus } from "@/lib/shape/torus"
 import { Vector3 } from "@/lib/math/vector"
@@ -42,11 +41,6 @@ export const onload = () => {
     gl.clearDepth(1.0)
 
     const program = new Program(gl, vertexSource, fragmentSource)
-
-    const attributes: Attribute[] = ["aVertexPosition", "aVertexColor"]
-    const uniforms: Uniform[] = ["uModelViewMatrix", "uProjectionMatrix"]
-
-    program.load(attributes, uniforms)
 
     scene = new Scene(gl, program)
     clock = new Clock()
