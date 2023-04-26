1c641a2373a02143281290567555edbea1cc3bad
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-04-26 19:14:35 +0900

    [radian] RawVector3の各要素をラジアンに変換するメソッド実装

--- src/lib/math/radian.ts
+++ src/lib/math/radian.ts
@@ -1,1 +1,7 @@
+import type { RawVector3 } from "./raw-vector"
+
 export const toRad = (angle: number) => (angle * Math.PI) / 180
+
+export const toRadMap3 = (angles: RawVector3): RawVector3 => {
+  return [toRad(angles[0]), toRad(angles[1]), toRad(angles[2])]
+}
