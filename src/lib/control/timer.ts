de155af7834cd1062f030dbc252a15833077df54
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-06-17 20:26:42 +0900

    Timer: updateしても合計時間を記録するように機能追加

--- src/lib/control/timer.ts
+++ src/lib/control/timer.ts
@@ -1,19 +1,30 @@
 export class Timer {
   private _startTime: number
+  private _totalTime: number
 
   constructor() {
     this._startTime = 0
+    this._totalTime = 0
   }
 
   start() {
     this._startTime = new Date().getTime()
   }
 
+  reset() {
+    this.start()
+  }
+
   update() {
+    this._totalTime += (new Date().getTime() - this._startTime) * 0.001
     this._startTime = new Date().getTime()
   }
 
   get elapsed() {
     return new Date().getTime() - this._startTime
+  }
+
+  get totalTime() {
+    return this._totalTime
   }
 }
