994ce2c9bfa2a8f33014aa969957a3757d11fdb0
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-13 14:25:35 +0900

    Wave Clock - step-1 円を描く

---
+++ src/canvas/p5-wave-clock-step-1/render.ts
@@ -0,0 +1,39 @@
+import p5 from "p5"
+
+const sketch = (p: p5) => {
+  let angle = -p.PI / 2
+  let radius = p.min(p.windowWidth, p.windowHeight) / 2 - 20
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+    p.frameRate(30)
+    p.background(255)
+    p.smooth()
+    p.noFill()
+  }
+
+  p.draw = () => {
+    angle += 3
+    if (angle > 360) angle -= 360
+    if (angle < 0) angle += 360
+
+    const centerX = p.windowWidth / 2
+    const centerY = p.windowHeight / 2
+
+    const rad = p.radians(angle)
+    const x1 = centerX + radius * p.cos(rad)
+    const y1 = centerY + radius * p.sin(rad)
+
+    // 反対の点を計算（ラジアンで180度回転）
+    const opprad = rad + p.PI
+    const x2 = centerX + radius * p.cos(opprad)
+    const y2 = centerY + radius * p.sin(opprad)
+
+    p.stroke(0, 60)
+    p.strokeWeight(1)
+    // 円周上のそれぞれの点から反対側の点に線を引く
+    p.line(x1, y1, x2, y2)
+  }
+}
+
+new p5(sketch)
