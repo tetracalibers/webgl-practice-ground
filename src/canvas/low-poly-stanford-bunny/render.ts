3e0d89d0362cf962aafa4419987a5b29d57cc7a0
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-26 09:38:28 +0900

    low-poly-stanford-bunny: SketchGl形式に

--- src/canvas/low-poly-stanford-bunny/render.ts
+++ src/canvas/low-poly-stanford-bunny/render.ts
@@ -1,13 +1,7 @@
-import type { RawVector3 } from "@/lib/math/raw-vector"
-import { Space } from "@/lib/canvas/index"
-import { Matrix4 } from "@/lib/math/matrix"
-import { Clock } from "@/lib/event/clock"
-import { Geometry } from "@/lib/webgl/geometry"
-import { Program } from "@/lib/webgl/gl-program"
-import { Uniforms } from "@/lib/webgl/gl-uniforms"
-import { AngleCamera } from "@/lib/camera/angle-camera"
-import { AngleCameraController } from "@/lib/control/angle-camera-controller"
-import { ControlUi } from "@/lib/gui/control-ui"
+import { SketchGl, SketchFn, SketchConfig } from "sketchgl"
+import { Matrix4, RawVector3 } from "sketchgl/math"
+import { Geometry, Program, Uniforms } from "sketchgl/webgl"
+import { AngleCamera, AngleCameraController } from "sketchgl/camera"
 
 import vert from "./index.vert?raw"
 import frag from "./index.frag?raw"
@@ -15,19 +9,10 @@ import frag from "./index.frag?raw"
 // https://www.thingiverse.com/thing:466857
 import rabbitModel from "@/model/json/Bunny-LowPoly.json" assert { type: "json" }
 
-export const onload = () => {
-  const space = new Space("gl-canvas")
-  const canvas = space.canvas
-  const gl = space.gl
-  if (!canvas || !gl) return
+const sketch: SketchFn = (skCanvas) => {
+  const { gl, canvas } = skCanvas
 
-  let program: Program
-  let clock: Clock
-  let camera: AngleCamera
-  let matP: Matrix4
-  let matM: Matrix4
-  let rabbit: Geometry
-  let materialColor: RawVector3 = [0.855, 0.792, 0.969]
+  const uMaterialColor: RawVector3 = [0.855, 0.792, 0.969]
 
   const uniforms = new Uniforms(gl, [
     "uMatView",
@@ -39,81 +24,70 @@ export const onload = () => {
     "uMaterialColor"
   ])
 
-  const initGuiControls = () => {
-    const ui = new ControlUi()
-    ui.rgb("Color", materialColor, (c) => {
-      uniforms.fvector3("uMaterialColor", c)
-    })
+  const program = new Program(gl)
+  program.attach(vert, frag)
+  program.activate()
+
+  const rabbit = new Geometry(gl)
+  rabbit.registAttrib({ location: 0, components: 3, buffer: new Float32Array(rabbitModel.vertices.map(Number)) })
+  rabbit.registAttrib({ location: 1, components: 3, buffer: new Float32Array(rabbitModel.normals.map(Number)) })
+  rabbit.registIndices(new Uint16Array(rabbitModel.indices))
+  rabbit.setup()
+
+  const camera = new AngleCamera("ORBIT")
+  camera.goHome([30, 30, 300])
+  camera.azimuth = 0
+  camera.elevation = 60
+  camera.focus = [0, 0, 0]
+  camera.update()
+
+  const matP = Matrix4.perspective(camera.fov, canvas.width / canvas.height, camera.near, camera.far)
+  const matM = Matrix4.identity()
+
+  AngleCameraController.init(canvas, camera)
+
+  uniforms.init(program.get())
+  uniforms.fmatrix4("uMatModel", matM.values)
+  uniforms.fmatrix4("uMatProj", matP.values)
+  uniforms.fmatrix4("uMatNormal", matM.inverse().values)
+  uniforms.fvector3("uLightDir", [-0.5, 0.5, 200])
+  uniforms.fvector3("uMaterialColor", uMaterialColor)
+  uniforms.fvector4("uAmbient", [0.2, 0.1, 0.2, 1.0])
+
+  gl.clearColor(1.0, 1.0, 1.0, 1.0)
+  gl.clearDepth(1.0)
+
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+
+  gl.enable(gl.CULL_FACE)
+
+  return {
+    drawOnFrame() {
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+      const matV = camera.View
+      uniforms.fmatrix4("uMatView", matV.values)
+
+      rabbit.bind()
+      rabbit.draw()
+    },
+    control(ui) {
+      ui.rgb("Color", uMaterialColor, (c) => {
+        uniforms.fvector3("uMaterialColor", c)
+      })
+    }
   }
+}
 
-  const onResize = () => {
-    space.fitScreenSquare()
-    render()
+export const onload = () => {
+  const config: SketchConfig = {
+    canvas: {
+      el: "gl-canvas",
+      fitSquare: true,
+      autoResize: true
+    }
   }
-
-  const configure = () => {
-    space.fitScreenSquare()
-
-    gl.clearColor(1.0, 1.0, 1.0, 1.0)
-    gl.clearDepth(1.0)
-
-    gl.enable(gl.DEPTH_TEST)
-    gl.depthFunc(gl.LEQUAL)
-
-    gl.enable(gl.CULL_FACE)
-
-    program = new Program(gl)
-    program.attach(vert, frag)
-    program.activate()
-
-    rabbit = new Geometry(gl)
-    rabbit.registAttrib({ location: 0, components: 3, buffer: new Float32Array(rabbitModel.vertices) })
-    rabbit.registAttrib({ location: 1, components: 3, buffer: new Float32Array(rabbitModel.normals) })
-    rabbit.registIndices(new Uint16Array(rabbitModel.indices))
-    rabbit.setup()
-
-    camera = new AngleCamera("ORBIT")
-    camera.goHome([30, 30, 300])
-    camera.azimuth = 0
-    camera.elevation = 60
-    camera.focus = [0, 0, 0]
-    camera.update()
-
-    matP = Matrix4.perspective(camera.fov, canvas.width / canvas.height, camera.near, camera.far)
-    matM = Matrix4.identity()
-
-    new AngleCameraController(canvas, camera)
-
-    uniforms.init(program.get())
-    uniforms.fmatrix4("uMatModel", matM.values)
-    uniforms.fmatrix4("uMatProj", matP.values)
-    uniforms.fmatrix4("uMatNormal", matM.inverse().values)
-    uniforms.fvector3("uLightDir", [-0.5, 0.5, 200])
-    uniforms.fvector3("uMaterialColor", materialColor)
-    uniforms.fvector4("uAmbient", [0.2, 0.1, 0.2, 1.0])
-
-    clock = new Clock()
-
-    space.onResize = onResize
-  }
-
-  const render = () => {
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-    const matV = camera.View
-    uniforms.fmatrix4("uMatView", matV.values)
-
-    rabbit.bind()
-    rabbit.draw()
-  }
-
-  const init = () => {
-    configure()
-    clock.on("tick", render)
-
-    initGuiControls()
-  }
-
-  init()
+  SketchGl.init(config, sketch)
 }
