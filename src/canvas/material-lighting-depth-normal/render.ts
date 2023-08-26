5b305c89d0eeacab48531d36687457efc6580e93
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-26 18:51:16 +0900

    material-lighting-depth-normal: SketchGlで書き換え

--- src/canvas/material-lighting-depth-normal/render.ts
+++ src/canvas/material-lighting-depth-normal/render.ts
@@ -1,15 +1,9 @@
-import type { RawVector3 } from "@/lib/math/raw-vector"
-import { Space } from "@/lib/canvas/index"
-import { Matrix4 } from "@/lib/math/matrix"
-import { Clock } from "@/lib/event/clock"
-import { Geometry } from "@/lib/webgl/geometry"
-import { Program } from "@/lib/webgl/gl-program"
-import { Uniforms } from "@/lib/webgl/gl-uniforms"
-import { AngleCamera } from "@/lib/camera/angle-camera"
-import { AngleCameraController } from "@/lib/control/angle-camera-controller"
-import { ControlUi } from "@/lib/gui/control-ui"
-import { generateHalfCoverPolygonData } from "@/model/generator/cover-polygon"
-import { MrtRenderer } from "@/lib/webgl/mrt-renderer"
+import { SketchGl, type SketchConfig, type SketchFn } from "sketchgl"
+import { Uniforms, Program } from "sketchgl/program"
+import { Matrix4, type RawVector3 } from "sketchgl/math"
+import { AngleCamera, AngleCameraController } from "sketchgl/camera"
+import { MRTRenderer } from "sketchgl/renderer"
+import { Geometry, HalfCanvasCoverPolygon } from "sketchgl/geometry"
 
 import vertCalc from "./calc.vert?raw"
 import fragCalc from "./calc.frag?raw"
@@ -18,150 +12,119 @@ import fragPreview from "./preview.frag?raw"
 
 import bunny from "@/model/json/Bunny-LowPoly.json" assert { type: "json" }
 
-export const onload = () => {
-  const space = new Space("gl-canvas")
-  const canvas = space.canvas
-  const gl = space.gl
-  if (!canvas || !gl) return
+const sketch: SketchFn = ({ gl, canvas }) => {
+  const uniforms = {
+    shape: new Uniforms(gl, [
+      "uMatView",
+      "uMatModel",
+      "uMatProj",
+      "uMatNormal",
+      "uLightDir",
+      "uAmbient",
+      "uMaterialColor"
+    ]),
+    plane: new Uniforms(gl, ["uOffset"])
+  }
 
-  let program: Program
-  let clock: Clock
-  let camera: AngleCamera
-  let renderer: MrtRenderer
-  let matP: Matrix4
-  let matM: Matrix4
-  let shape: Geometry
-  let plane: Geometry
-  let materialColor: RawVector3 = [0.792, 0.824, 0.969]
+  const program = new Program(gl)
+  program.attach(vertPreview, fragPreview)
 
-  const planeOffset = [
+  const camera = new AngleCamera("ORBIT")
+  camera.goHome([45, 30, 300])
+  camera.azimuth = -5
+  camera.elevation = 30
+  camera.focus = [0, 0, 0]
+  camera.update()
+
+  AngleCameraController.init(canvas, camera)
+
+  const renderer = new MRTRenderer(gl, canvas, vertCalc, fragCalc, { texCount: 4 })
+
+  const planeOffsets = [
     [-0.5, -0.5, 0.0],
     [-0.5, 0.5, 0.0],
     [0.5, -0.5, 0.0],
     [0.5, 0.5, 0.0]
   ]
+  let uMaterialColor: RawVector3 = [0.792, 0.824, 0.969]
+  let matP = Matrix4.perspective(camera.fov, canvas.width / canvas.height, camera.near, camera.far)
+  let matM: Matrix4
+  let matV: Matrix4
 
-  const uniformsShape = new Uniforms(gl, [
-    "uMatView",
-    "uMatModel",
-    "uMatProj",
-    "uMatNormal",
-    "uLightDir",
-    "uAmbient",
-    "uMaterialColor"
-  ])
+  uniforms.shape.init(renderer.program.get())
+  uniforms.plane.init(program.get())
 
-  const uniformsPlane = new Uniforms(gl, ["uOffset"])
+  const shape = new Geometry(gl)
+  shape.registAttrib({ location: 0, components: 3, buffer: new Float32Array(bunny.vertices.map(Number)) })
+  shape.registAttrib({ location: 1, components: 3, buffer: new Float32Array(bunny.normals.map(Number)) })
+  shape.registIndices(new Uint16Array(bunny.indices))
+  shape.setup()
 
-  const initGuiControls = () => {
-    const ui = new ControlUi()
-    ui.rgb("Color", materialColor, (c) => (materialColor = c))
-  }
+  const plane = new HalfCanvasCoverPolygon(gl)
+  plane.create({ vertices: 0, uv: 1 })
 
-  const onResize = () => {
-    space.fitScreenSquare()
-    renderer.resize()
-    render()
-  }
+  gl.clearColor(1.0, 1.0, 1.0, 1.0)
+  gl.clearDepth(1.0)
 
-  const configure = () => {
-    space.fitScreenSquare()
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
 
-    gl.clearColor(1.0, 1.0, 1.0, 1.0)
-    gl.clearDepth(1.0)
+  gl.enable(gl.CULL_FACE)
 
-    gl.enable(gl.DEPTH_TEST)
-    gl.depthFunc(gl.LEQUAL)
+  return {
+    resize: [renderer.resize],
 
-    gl.enable(gl.CULL_FACE)
+    drawOnFrame: () => {
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
-    program = new Program(gl)
-    program.attach(vertPreview, fragPreview)
+      renderer.switchToOffcanvas()
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-    renderer = new MrtRenderer(gl, canvas, vertCalc, fragCalc, { texCount: 4 })
+      matV = camera.View
+      uniforms.shape.fmatrix4("uMatView", matV.values)
+      uniforms.shape.fmatrix4("uMatProj", matP.values)
+      uniforms.shape.fvector3("uLightDir", [-0.5, 0.5, 200])
+      uniforms.shape.fvector3("uMaterialColor", uMaterialColor)
+      uniforms.shape.fvector4("uAmbient", [0.2, 0.1, 0.2, 1.0])
 
-    camera = new AngleCamera("ORBIT")
-    camera.goHome([45, 30, 300])
-    camera.azimuth = -5
-    camera.elevation = 30
-    camera.focus = [0, 0, 0]
-    camera.update()
+      shape.bind()
 
-    matP = Matrix4.perspective(camera.fov, canvas.width / canvas.height, camera.near, camera.far)
+      matM = Matrix4.translation(-30, 30, -500)
+      uniforms.shape.fmatrix4("uMatModel", matM.values)
+      uniforms.shape.fmatrix4("uMatNormal", matM.inverse().values)
+      shape.draw({ primitive: "TRIANGLES" })
 
-    new AngleCameraController(canvas, camera)
+      matM = Matrix4.translation(15, 15, 0)
+      uniforms.shape.fmatrix4("uMatModel", matM.values)
+      uniforms.shape.fmatrix4("uMatNormal", matM.inverse().values)
+      shape.draw({ primitive: "TRIANGLES" })
 
-    uniformsShape.init(renderer.program.get())
-    uniformsPlane.init(program.get())
+      renderer.switchToCanvas(program)
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-    clock = new Clock()
+      for (let i = 0; i < 4; ++i) {
+        uniforms.plane.fvector3("uOffset", planeOffsets[i])
+        renderer.useAsTexture(i, "uTexture", program.get())
+        plane.bind()
+        plane.draw({ primitive: "TRIANGLES" })
+      }
+    },
 
-    space.onResize = onResize
-  }
-
-  const setupGeometry = () => {
-    const model = bunny
-    shape = new Geometry(gl)
-    shape.registAttrib({ location: 0, components: 3, buffer: new Float32Array(model.vertices) })
-    shape.registAttrib({ location: 1, components: 3, buffer: new Float32Array(model.normals) })
-    shape.registIndices(new Uint16Array(model.indices))
-    shape.setup()
-
-    const planeData = generateHalfCoverPolygonData()
-    plane = new Geometry(gl)
-    plane.registAttrib({ location: 0, components: 3, buffer: new Float32Array(planeData.vertices) })
-    plane.registAttrib({ location: 1, components: 2, buffer: new Float32Array(planeData.texCoord) })
-    plane.registIndices(new Uint16Array(planeData.indices))
-    plane.setup()
-  }
-
-  const render = () => {
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-    renderer.switchToOffcanvas()
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-    const matV = camera.View
-    uniformsShape.fmatrix4("uMatView", matV.values)
-    uniformsShape.fmatrix4("uMatProj", matP.values)
-    uniformsShape.fvector3("uLightDir", [-0.5, 0.5, 200])
-    uniformsShape.fvector3("uMaterialColor", materialColor)
-    uniformsShape.fvector4("uAmbient", [0.2, 0.1, 0.2, 1.0])
-
-    shape.bind()
-
-    matM = Matrix4.translation(-30, 30, -500)
-    uniformsShape.fmatrix4("uMatModel", matM.values)
-    uniformsShape.fmatrix4("uMatNormal", matM.inverse().values)
-
-    shape.draw({ primitive: "TRIANGLES" })
-
-    matM = Matrix4.translation(15, 15, 0)
-    uniformsShape.fmatrix4("uMatModel", matM.values)
-    uniformsShape.fmatrix4("uMatNormal", matM.inverse().values)
-
-    shape.draw({ primitive: "TRIANGLES" })
-
-    renderer.switchToCanvas(program)
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-    for (let i = 0; i < 4; ++i) {
-      uniformsPlane.fvector3("uOffset", planeOffset[i])
-      renderer.useTextureOf(i, "uTexture", program.get())
-      plane.bind()
-      plane.draw({ primitive: "TRIANGLES" })
+    control: (ui) => {
+      ui.rgb("Color", uMaterialColor, (c) => {
+        uMaterialColor = c
+      })
     }
   }
+}
 
-  const init = () => {
-    configure()
-    setupGeometry()
-    clock.on("tick", render)
-
-    initGuiControls()
+export const onload = () => {
+  const config: SketchConfig = {
+    canvas: {
+      el: "gl-canvas",
+      fitSquare: true,
+      autoResize: true
+    }
   }
-
-  init()
+  SketchGl.init(config, sketch)
 }
