ce10c3cc04eaed59f501a8526794c8f67fd67da9
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-08-28 22:13:16 +0900

    periodic-noise-2to1: SketchGlで書き換え

--- src/canvas/periodic-noise-2to1/render.ts
+++ src/canvas/periodic-noise-2to1/render.ts
@@ -1,59 +1,40 @@
-import { Space } from "@/lib/canvas/index"
-import { Program } from "@/lib/webgl/program"
-import { Timer } from "@/lib/control/timer"
-import { Clock } from "@/lib/event/clock"
-import { UniformLoader } from "@/lib/webgl/uniform-loader"
+import { SketchFrg, type FragmentSketchConfig, type FragmentSketchFn } from "sketchgl"
+import { Timer } from "sketchgl/interactive"
+import { Uniforms } from "sketchgl/program"
 
-import vert from "./index.vert?raw"
 import frag from "./index.frag?raw"
 
+const sketch: FragmentSketchFn = ({ gl, canvas, program, renderToCanvas }) => {
+  const uniforms = new Uniforms(gl, ["uResolution", "uTime"])
+  uniforms.init(program)
+
+  const timer = new Timer()
+  timer.start()
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.clearDepth(1.0)
+
+  return {
+    drawOnFrame() {
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+      uniforms.float("uTime", timer.elapsed * 0.001)
+      uniforms.fvector2("uResolution", [canvas.width, canvas.height])
+
+      renderToCanvas()
+    }
+  }
+}
+
 export const onload = () => {
-  const space = new Space("gl-canvas")
-  const canvas = space.canvas
-  const gl = space.gl
-  if (!canvas || !gl) return
-
-  let timer: Timer
-  let clock: Clock
-
-  const uniforms = new UniformLoader(gl, ["uResolution", "uTime"])
-
-  const onResize = () => {
-    space.fitScreenSquare()
-    render()
+  const config: FragmentSketchConfig = {
+    frag,
+    canvas: {
+      el: "gl-canvas",
+      fitSquare: true,
+      autoResize: true
+    }
   }
-
-  const configure = () => {
-    space.fitScreenSquare()
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0)
-    gl.clearDepth(1.0)
-
-    const program = new Program(gl, vert, frag)
-
-    clock = new Clock()
-    timer = new Timer()
-
-    uniforms.init(program)
-
-    timer.start()
-    space.onResize = onResize
-  }
-
-  const render = () => {
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-    uniforms.float("uTime", timer.elapsed * 0.001)
-    uniforms.fvector2("uResolution", [canvas.width, canvas.height])
-
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 3)
-  }
-
-  const init = () => {
-    configure()
-    clock.on("tick", render)
-  }
-
-  init()
+  SketchFrg.init(config, sketch)
 }
