34aec87af0af50bb1df1d09da2668e5ada1c61ad
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-09-03 08:05:10 +0900

    alive-particle-random-blizzard: SketchGlで書き換え

--- src/canvas/alive-particle-random-blizzard/render.ts
+++ src/canvas/alive-particle-random-blizzard/render.ts
@@ -1,9 +1,9 @@
-import { Space } from "@/lib/canvas/index"
-import { Clock } from "@/lib/event/clock"
-import { Timer } from "@/lib/control/timer"
-import { SwapTransformFeedback } from "@/lib/webgl/swap-transform-feedback"
+import { SketchGl, type SketchConfig, type SketchFn } from "sketchgl"
+import { InterleavedInitialData } from "sketchgl/utility"
+import { Timer } from "sketchgl/interactive"
+import { SwapTFRenderer } from "sketchgl/renderer"
+import { Uniforms } from "sketchgl/program"
 import { AliveParticlesSystem } from "@/lib/feature/particle/alive-particle"
-import { UniformManager } from "@/lib/webgl/uniform-manager"
 
 import vertForUpdate from "./update.vert?raw"
 import fragForUpdate from "./update.frag?raw"
@@ -11,103 +11,29 @@ import fragForUpdate from "./update.frag?raw"
 import vertForRender from "./render.vert?raw"
 import fragForRender from "./render.frag?raw"
 
-/**
-
-## configure
-
-1. 最大数パーティクルに必要なデータを保持するのに十分な大きさの、同じサイズの2つのバッファを作成
-2. そのうちの1つを「読み取り」バッファーとして指定し、もう1つを「書き込み」バッファーとして指定
-3. 「読み取り」バッファーにパーティクルシステムの初期状態を入力
-
-## render
-
-1. 「読み取り」バッファを入力としてバインドし、「書き込みバッファ」をTransformFeedbackバッファとしてバインド
-2. updateプログラムを呼び出して、パーティクルの更新された状態をTransformFeedbackバッファに書き込み
-3. 「書き込み」バッファを入力としてバインドし、Renderプログラムを呼び出して、パーティクルの更新された状態を画面上に表示
-4. 「読み取り」バッファと「書き込み」バッファを交換
-
-**/
-
-export const onload = () => {
-  const space = new Space("gl-canvas")
-  const canvas = space.canvas
-  const gl = space.gl
-  if (!canvas || !gl) return
-
-  let clock: Clock
-  let tf: SwapTransformFeedback
-  let programForUpdate: WebGLProgram
-  let programForRender: WebGLProgram
-  let uniformsForUpdate: UniformManager
-  let timer: Timer
-  let particles: AliveParticlesSystem
-
+const sketch: SketchFn = ({ gl, canvas }) => {
   const particlesCount = 100000
   const particleMinAge = 1.01
   const particleMaxAge = 5.15
 
-  // パーティクルシステムの初期状態を表すデータをインタリーブ配列として生成
-  // 最初に1回GPUに送信され、以降はUpdateシェーダーによって更新される
-  const initialParticleData = (count: number, minAge: number, maxAge: number) => {
-    const data: number[] = []
-    for (let i = 0; i < count; ++i) {
-      // position
-      data.push(0.0)
-      data.push(0.0)
-
-      const life = minAge + Math.random() * (maxAge - minAge)
-      // age
-      data.push(life + 1)
-      data.push(life)
-
-      // velocity
-      data.push(0.0)
-      data.push(0.0)
+  const initialData = new InterleavedInitialData(() => {
+    const life = particleMinAge + Math.random() * (particleMaxAge - particleMinAge)
+    return {
+      vPosition: [0, 0],
+      vAge: life + 1,
+      vLife: life,
+      vVelocity: [0, 0]
     }
-    return data
-  }
+  })
+  const interleaved = initialData.generate({ count: particlesCount })
 
-  const onResize = () => {
-    space.fitScreen()
-    render()
-  }
+  // TODO: ライブラリでうまいこと提供する
+  // TODO: interleaved.length / 6 を隠蔽する
+  const particles = new AliveParticlesSystem(canvas, interleaved.length / 6)
+  particles.gravity = [0, 0]
 
-  const configure = () => {
-    space.fitScreen()
-
-    timer = new Timer()
-
-    tf = new SwapTransformFeedback(gl)
-
-    // varyingsはインタリーブ配列と同じ順で
-    programForUpdate = tf.createProgram(vertForUpdate, fragForUpdate, ["vPosition", "vAge", "vLife", "vVelocity"])!
-    programForRender = tf.createProgram(vertForRender, fragForRender)!
-
-    // インタリーブ配列と同じ順で
-    tf.registUpdateAttrib(0, 2) // aPosition
-    tf.registUpdateAttrib(2, 1) // aAge
-    tf.registUpdateAttrib(3, 1) // aLife
-    tf.registUpdateAttrib(1, 2) // aVelocity
-
-    // インタリーブ配列と同じ順で
-    tf.registRenderAttrib(0, 2) // aPosition
-    tf.registRenderAttrib(2, 1) // aAge
-    tf.registRenderAttrib(3, 1) // aLife
-
-    const initialData = new Float32Array(initialParticleData(particlesCount, particleMinAge, particleMaxAge))
-
-    tf.setupDataAndAttrib(initialData)
-
-    particles = new AliveParticlesSystem(canvas, initialData.length / 6)
-    particles.gravity = [0, 0]
-
-    gl.enable(gl.BLEND)
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0)
-
-    uniformsForUpdate = new UniformManager(gl, programForUpdate)
-    uniformsForUpdate.init([
+  const uniformsFor = {
+    update: new Uniforms(gl, [
       "uTime",
       "uTimeDelta",
       "uGravity",
@@ -117,48 +43,75 @@ export const onload = () => {
       "uMinSpeed",
       "uMaxSpeed"
     ])
-
-    clock = new Clock()
-
-    timer.start()
-    space.onResize = onResize
   }
 
-  const render = () => {
-    const timeDelta = timer.elapsed
+  const renderer = new SwapTFRenderer(gl, initialData.keys)
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+  renderer.attachUpdateProgram(vertForUpdate, fragForUpdate)
+  renderer.attachRenderProgram(vertForRender, fragForRender)
 
-    gl.useProgram(programForUpdate)
+  renderer.registUpdateAttrib("vPosition", { location: 0, components: 2 })
+  renderer.registUpdateAttrib("vAge", { location: 1, components: 1 })
+  renderer.registUpdateAttrib("vLife", { location: 2, components: 1 })
+  renderer.registUpdateAttrib("vVelocity", { location: 3, components: 2 })
 
-    uniformsForUpdate.float("uTime", timer.totalTime)
-    uniformsForUpdate.float("uTimeDelta", timeDelta * 0.001)
-    uniformsForUpdate.fvector2("uGravity", particles.gravity)
-    uniformsForUpdate.fvector2("uOrigin", particles.origin)
-    uniformsForUpdate.float("uMinTheta", particles.minTheta)
-    uniformsForUpdate.float("uMaxTheta", particles.maxTheta)
-    uniformsForUpdate.float("uMinSpeed", particles.minSpeed)
-    uniformsForUpdate.float("uMaxSpeed", particles.maxSpeed)
+  renderer.registRenderAttrib("aPosition", { location: 0, components: 2 })
+  renderer.registRenderAttrib("aAge", { location: 1, components: 1 })
+  renderer.registRenderAttrib("aLife", { location: 2, components: 1 })
 
-    tf.startUpdate()
-    gl.drawArrays(gl.POINTS, 0, particles.alives)
-    tf.endUpdate()
+  renderer.setup(new Float32Array(interleaved))
 
-    gl.useProgram(programForRender)
+  uniformsFor.update.init(renderer.glProgramForUpdate)
 
-    tf.startRender()
-    gl.drawArrays(gl.POINTS, 0, particles.alives)
-    tf.endRender()
+  const timer = new Timer()
+  timer.start()
 
-    timer.update()
-    particles.updateForNext(timeDelta)
+  gl.enable(gl.BLEND)
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+  return {
+    drawOnFrame() {
+      const deltaTime = timer.elapsed
+
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+      renderer.startUpdate()
+
+      uniformsFor.update.float("uTime", timer.totalTime)
+      uniformsFor.update.float("uTimeDelta", deltaTime * 0.001)
+      uniformsFor.update.fvector2("uGravity", particles.gravity)
+      uniformsFor.update.fvector2("uOrigin", particles.origin)
+      uniformsFor.update.float("uMinTheta", particles.minTheta)
+      uniformsFor.update.float("uMaxTheta", particles.maxTheta)
+      uniformsFor.update.float("uMinSpeed", particles.minSpeed)
+      uniformsFor.update.float("uMaxSpeed", particles.maxSpeed)
+
+      gl.drawArrays(gl.POINTS, 0, particles.alives)
+
+      renderer.endUpdate()
+
+      renderer.startRender()
+
+      gl.drawArrays(gl.POINTS, 0, particles.alives)
+
+      renderer.endRender()
+
+      timer.update()
+      particles.updateForNext(deltaTime)
+    }
   }
+}
 
-  const init = () => {
-    configure()
-    clock.on("tick", render)
+export const onload = () => {
+  const config: SketchConfig = {
+    canvas: {
+      el: "gl-canvas",
+      fit: "square",
+      autoResize: true
+    }
   }
-
-  init()
+  SketchGl.init(config, sketch)
 }
